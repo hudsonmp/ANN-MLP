490a186813bbe91a184e645867dde439afd54274
hudsonmp <hudsonmitchellpullman@gmail.com>
2025-02-15 12:14:21 -1000

    fixed imports

--- utils/imports.py
+++ utils/imports.py
@@ -8,6 +8,12 @@ import matplotlib.pyplot as plt
 # Machine Learning
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
 
 # System utilities
 import os
